291f556d765eb07d276ebaafa130dfef41124e53
Michael Antczak <Michael.Antczak@gmail.com>
2017-11-23 18:45:33 +0000

    Fetch users from Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import firebase from './config/firebase.js';
+import ListUsers from './components/ListUsers.js'
 
 class App extends Component {
 
@@ -17,31 +18,45 @@ class App extends Component {
     // Get a reference to the database service
     const database = firebase.database();
 
-    // fetch users
-    let users = database.ref('users/');
-    users.on('value', (snapshot) => {
-      console.log(snapshot.val());
-      this.setState({
-        users: snapshot.val()
-      });
-    });
+    // fetch users from Firebase
+    const users = database.ref('users');
 
-    // fetch accounts
-    let accounts = database.ref('accounts/');
-    accounts.on('value', (s) => {
-      console.log(s.val());
+    // this sets an event on change in Firebase, so 
+    // will be updated automagically when there is 
+    // an update in Firebase
+    users.on('value', (snapshot) => {
+
+      // convert Firebase object into an Array and add to state
+      const allUsers = snapshot.val();
+      let newState = [];
+
+      for(let user_id in allUsers) {
+        newState.push({
+          id: user_id,
+          name: allUsers[user_id].name,
+          account: allUsers[user_id].account,
+        });
+        // DEBUG ONLY console.log(`ID: ${user_id}, NAME: ${allUsers[user_id].name}`)
+      }
+ 
       this.setState({
-        accounts: s.val()
+        users: newState
       });
     });
   }
   
   render() {
+
+    const message = (!this.state.users) ? "Loading..." : <ListUsers users={this.state.users} />
+    
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Beezer</h1>
+          <h1 className="App-title">Welcome to Beezer admin panel</h1>
         </header>
+          
+          {message}
+
       </div>
     );
   }
