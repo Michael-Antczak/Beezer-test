8443e3bb4cdeb89b32403761d4f1b03730ac6563
Michael Antczak <Michael.Antczak@gmail.com>
2017-11-24 13:00:36 +0000

    Update ListUsers w/ search

--- src/components/ListUsers.js
+++ src/components/ListUsers.js
@@ -2,27 +2,52 @@ import React from 'react';
 import User from './User';
 import './ListUsers.css';
 
-const ListUsers = (props) => {
+class ListUsers extends React.Component {
 
-    const users = props.users;
-    const accounts = props.accounts;
+    constructor(props) {
+        super(props);
+        this.state = {
+          searchTerm: ''
+        }
+        this.handleSearchTermChange = this.handleSearchTermChange.bind(this);        
+    }
 
-    return (
+    handleSearchTermChange(e) {
+        this.setState({
+          searchTerm: e.target.value,
+        });
+    }
 
-        <div id="list-users">
-            <h1>List of Users</h1> 
-            <ul id="user-list">
-                {
-                    users.map((user) => {
+    render() {
 
-                        return (
-                            <User key={user.id} user={user} accounts={accounts}/>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    )
+        let users = this.props.users;
+        let accounts = this.props.accounts;
+
+        return (
+            
+            <div id="list-users">
+                <h1>List of Users</h1> 
+                <h1 className="search">
+                    <input 
+                        onChange={this.handleSearchTermChange} 
+                        value={this.state.searchTerm} 
+                        type="text" 
+                        placeholder="Search..." 
+                    />
+                </h1>
+                <ul id="user-list">
+
+                    {users
+                        .filter(user => (user.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())) )
+                        .map((user) => <User key={user.id} user={user} accounts={accounts}/> )
+                    }
+
+                </ul>
+            </div>
+            
+        );
+    }
+    
 }
 
 export default ListUsers;
